cc67a26e6038c1723bfe913794c4bd9c522f7401
zppzdd <1120383256@qq.com>
2019-10-11 18:11:48 +0800

    大事件

---
+++ web_back/tool/config.js
@@ -0,0 +1,10 @@
+// 这个文件中用来保存每个页面中使用的数据信息，例如接口地址
+// - baseUrl中保存的是基地址
+var baseUrl = 'http://localhost:8000';
+
+
+// - 后面的变量中保存的是每个接口的地址
+var USER_LOGIN = baseUrl + '/admin/login';
+var USER_LOGOUT = baseUrl + '/admin/logout';
+var USER_GETINFO = baseUrl + '/admin/getuser';
+
